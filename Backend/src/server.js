478722ed8db2a653867908267842cce91bf7be72
shivam-mishra-music <shivammishra.an5@gmail.com>
2025-12-06 12:05:37 +0530

    Next.js version updated

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,4 +5,4 @@ import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
